3ae4f93488bbbbcad1818406e7bb55bdf14c4e44
klim0506 <56505440+klim0506@users.noreply.github.com>
2022-10-01 11:23:44 +0300

    Update analiz.py

--- data/info/analiz.py
+++ data/info/analiz.py
@@ -1,18 +1,40 @@
 import pandas
 import pymorphy2
 
-data = pandas.read_csv('/small_data/small_data/Контракты 44ФЗ.csv', sep=';')
 
-data['root_words'] = ''
+def parsing_names(path):
+    data = pandas.read_csv(path, sep=';')
 
-shcet = 0
-morphy = pymorphy2.MorphAnalyzer()
+    data['root_words'] = ''
 
-for i in list(data.product_name):
-    stroca = ''
-    for j in i.split():
-        j = morphy.parse(j)[0]
-        stroca += j.normal_form + ' '
+    shcet = 0
+    morphy = pymorphy2.MorphAnalyzer()
 
-    data.root_words[shcet] = stroca
-    shcet += 1
+    for i in list(data.product_name):
+        stroca = ''
+        for j in i.split():
+            j = morphy.parse(j)[0]
+            stroca += j.normal_form + ' '
+
+        data.root_words[shcet] = stroca
+        shcet += 1
+
+    return 0
+
+
+def parsing_zapros(string):
+    parser = pymorphy2.MorphAnalyzer(lang='ru')
+
+    zapros_param = {'NOUN': [], 'ADJF': [], 'ELSE': []}
+
+    for i in string.split():
+        chast_rechi = parser.parse(i)[0].tag.POS
+
+        if chast_rechi == 'PRTF':
+            zapros_param['ADJF'].append(i)
+        elif chast_rechi != 'NOUN' and chast_rechi != 'ADJF':
+            zapros_param['ELSE'].append(i)
+        else:
+            zapros_param[chast_rechi].append(i)
+
+    return zapros_param
